e696b7449c58cec0cbcdd81b52b504747f82fca4
Alex Stolbov <100lbov21@gmail.com>
2025-11-06 22:29:11 +0400

    feat: decide task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../lib');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let firstLetter = '';
+  for (let i = 0; i < members.length; i += 1) {
+    let item = members[i];
+    if (typeof item !== 'string') continue;
+      let str = item.trimStart();
+      firstLetter += str.charAt(0).toUpperCase();
+    }
+  let arrFirstLetter = firstLetter.split('').sort().join('');
+  return arrFirstLetter;
 }
 
 module.exports = {
